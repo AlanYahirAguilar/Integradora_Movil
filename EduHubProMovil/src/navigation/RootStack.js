e36ae69698f5d78cd0c71e467a610e38d99cd487
AlanYahirAguilar <alanyr107@gmail.com>
2025-04-18 16:09:28 -0600

    Detalles

--- EduHubProMovil/src/navigation/RootStack.js
+++ EduHubProMovil/src/navigation/RootStack.js
@@ -1,29 +1,22 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '../modules/homeScreen/HomeScreen';
-import SplashScreen from '../modules/splash/SplashScreen';
-import CourseDetailScreen from '../modules/courseDetailScreen/CourseDetailScreen';
-import SignIn from '../modules/signIn/signIn';
-import SignUp from '../modules/signUp/signUp';
 import CustomHeader from '../components/CustomHeader';
-import MyCoursesScreen from '../modules/myCourses/MyCoursesScreen';
+import CourseModuleDetails from '../components/NUEVOOOOOS/CourseModuleDetails';
+import VoucherVerification from '../components/NUEVOOOOOS/VoucherVerification';
 import PaymentInfoScreen from '../components/PaymentInfoScreen';
 import PendingEnrollmentsScreen from '../components/PendingEnrollmentsScreen';
-import VoucherVerification from '../components/NUEVOOOOOS/VoucherVerification'
-import CourseModuleDetails from '../components/NUEVOOOOOS/CourseModuleDetails';
-import Profile from '../modules/profile/Profile';
-import CourseCompletionScreen from '../components/CompletionScreen';
-import LessonScreen from '../modules/lessonScreen/LessonProgressScreen';
-import CategoryInformatica from '../modules/categories/CategoryInformatica'; 
-import CategoryDesarrolloWeb from '../modules/categories/CategoryDesarrolloWeb';
-import CategoryIOT from '../modules/categories/CategoryIOT';
-import CategoryProgramacion from '../modules/categories/CategoryProgramacion';
-import CategoryComunicacion from '../modules/categories/CategoryComunicacion';
-import CategoryScreen from '../modules/categories/CategoryScreen';
 import AdvancedSearch from '../modules/advancedSearch/AdvancedSearch';
+import CategoryScreen from '../modules/categories/CategoryScreen';
+import CourseDetailScreen from '../modules/courseDetailScreen/CourseDetailScreen';
+import HomeScreen from '../modules/homeScreen/HomeScreen';
+import MyCoursesScreen from '../modules/myCourses/MyCoursesScreen';
+import MyInscriptionsCourses from '../modules/myCourses/MyInscriptionsCourses';
+import Profile from '../modules/profile/Profile';
 import RecoverPassword from '../modules/recoverPassword/recoverPassword';
-import VerificationCodeScreen from '../modules/recoverPassword/VerificationCodeScreen';
 import ResetPasswordScreen from '../modules/recoverPassword/ResetPasswordScreen';
-import MyInscriptionsCourses from '../modules/myCourses/MyInscriptionsCourses'; 
+import VerificationCodeScreen from '../modules/recoverPassword/VerificationCodeScreen';
+import SignIn from '../modules/signIn/signIn';
+import SignUp from '../modules/signUp/signUp';
+import SplashScreen from '../modules/splash/SplashScreen';
 // Importar los nuevos componentes
 import LessonDetail from '../components/NUEVOOOOOS/LessonDetail';
 import ModuleSections from '../components/NUEVOOOOOS/ModuleSections';
@@ -33,7 +26,7 @@ const Stack = createStackNavigator();
 
 export const RootStack = ({ initialToken }) => {
   const initialRouteName = initialToken ? 'Home' : 'carga';
-  
+
   return (
     <Stack.Navigator initialRouteName={initialRouteName}>
       {/* Pantallas sin header */}
@@ -42,87 +35,88 @@ export const RootStack = ({ initialToken }) => {
       <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
 
       {/* Pantallas con header personalizado */}
-      <Stack.Screen 
-        name="Home" 
-        component={HomeScreen} 
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
         options={({ navigation }) => ({
           header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />
-        })} 
+        })}
       />
-      <Stack.Screen name="perfil" component={Profile} options={({ navigation }) => ({ header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />
-        })} 
-      /> 
-      <Stack.Screen 
-        name="CourseDetail" 
-        component={CourseDetailScreen} 
+      <Stack.Screen name="perfil" component={Profile} options={({ navigation }) => ({
+        header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />
+      })}
+      />
+      <Stack.Screen
+        name="CourseDetail"
+        component={CourseDetailScreen}
         options={({ navigation }) => ({
           header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />,
           title: 'Detalles del Curso'
-        })} 
+        })}
       />
       <Stack.Screen name='En curso' component={MyCoursesScreen} options={({ navigation }) => ({
-          header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />
-        })} 
-        />
-        <Stack.Screen name='Inscritos' component={MyInscriptionsCourses} options={({ navigation }) => ({
-          header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />
-        })} 
-        />
+        header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />
+      })}
+      />
+      <Stack.Screen name='Inscritos' component={MyInscriptionsCourses} options={({ navigation }) => ({
+        header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />
+      })}
+      />
 
       {/* Nuevas pantallas agregadas */}
-      <Stack.Screen 
-        name="PaymentInfo" 
-        component={PaymentInfoScreen} 
+      <Stack.Screen
+        name="PaymentInfo"
+        component={PaymentInfoScreen}
         options={({ navigation }) => ({
           header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />,
           title: 'Información de Pago',
-        })} 
+        })}
       />
-      <Stack.Screen 
-        name="PendingEnrollments" 
-        component={PendingEnrollmentsScreen} 
+      <Stack.Screen
+        name="PendingEnrollments"
+        component={PendingEnrollmentsScreen}
         options={({ navigation }) => ({
           header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />,
           title: 'Inscripciones Pendientes',
-        })} 
+        })}
       />
       {/* Ruta directa a la pantalla de verificación de voucher */}
-      <Stack.Screen 
-        name="voucher-verification" 
-        component={VoucherVerification} 
+      <Stack.Screen
+        name="voucher-verification"
+        component={VoucherVerification}
         options={({ navigation }) => ({
           header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />,
           title: 'Verificación de Voucher',
-        })} 
+        })}
       />
-      
+
       {/* Ruta directa a la pantalla de detalles del módulo */}
-      <Stack.Screen 
-        name="course-module-details" 
-        component={CourseModuleDetails} 
+      <Stack.Screen
+        name="course-module-details"
+        component={CourseModuleDetails}
         options={({ navigation }) => ({
           header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />,
           title: 'Detalles del Módulo',
-        })} 
+        })}
       />
 
       {/* Nuevas pantallas para el detalle de lecciones y secciones */}
-      <Stack.Screen 
-        name="LessonDetail" 
-        component={LessonDetail} 
+      <Stack.Screen
+        name="LessonDetail"
+        component={LessonDetail}
         options={({ navigation, route }) => ({
           header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />,
           title: route.params?.sectionName || 'Detalle de Lección',
-        })} 
+        })}
       />
 
-      <Stack.Screen 
-        name="ModuleSections" 
-        component={ModuleSections} 
+      <Stack.Screen
+        name="ModuleSections"
+        component={ModuleSections}
         options={({ navigation, route }) => ({
           header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />,
           title: `Módulo ${route.params?.moduleName || ''}`,
-        })} 
+        })}
       />
 
       {/* <Stack.Screen
@@ -134,7 +128,6 @@ export const RootStack = ({ initialToken }) => {
       })}
     />
 
-
     <Stack.Screen
     name="lecciones_Completado"
     component={CourseCompletionScreenn}
@@ -142,75 +135,35 @@ export const RootStack = ({ initialToken }) => {
     title: 'Detalles del Módulo',})}
     /> */}
 
-    <Stack.Screen 
-    name="informatica"
-    component={CategoryInformatica}
-    options={({ navigation }) => ({
-      header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />,
-      title: 'Informatica',
-    })}/>
+      <Stack.Screen
+        name="Category"
+        component={CategoryScreen}
+        options={({ navigation, route }) => ({
+          header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />,
+          title: route.params?.categoryName || 'Categoría',
+        })} />
 
-<Stack.Screen 
-    name="desarrollo"
-    component={CategoryDesarrolloWeb}
-    options={({ navigation }) => ({
-      header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />,
-      title: 'Desarrollo Web',
-    })}/>
+      <Stack.Screen
+        name='busquedaAvanzada'
+        component={AdvancedSearch}
+        options={({ navigation }) => ({
+          header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />
+        })} />
 
-<Stack.Screen 
-    name="programacion"
-    component={CategoryProgramacion}
-    options={({ navigation }) => ({
-      header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />,
-      title: 'Programacion',
-    })}/>
+      <Stack.Screen
+        name='recoverPass'
+        component={RecoverPassword}
+        options={{ headerShown: false }} />
 
-<Stack.Screen 
-    name="iot"
-    component={CategoryIOT}
-    options={({ navigation }) => ({
-      header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />,
-      title: 'IOT',
-    })}/>
+      <Stack.Screen
+        name='verificacionCode'
+        component={VerificationCodeScreen}
+        options={{ headerShown: false }} />
 
-<Stack.Screen 
-    name="comunicacion"
-    component={CategoryComunicacion}
-    options={({ navigation }) => ({
-      header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />,
-      title: 'Comunicacion',
-    })}/>
-
-<Stack.Screen 
-    name="Category"
-    component={CategoryScreen}
-    options={({ navigation, route }) => ({
-      header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />,
-      title: route.params?.categoryName || 'Categoría',
-    })}/>
-
-    <Stack.Screen
-    name='busquedaAvanzada'
-    component={AdvancedSearch}
-    options={({ navigation }) => ({
-      header: () => <CustomHeader toggleSidebar={() => navigation.setParams({ toggleSidebar: true })} />
-    })}/>
-
-    <Stack.Screen
-    name='recoverPass'
-    component={RecoverPassword} 
-    options={{headerShown: false}}/>
-
-    <Stack.Screen
-    name='verificacionCode'
-    component={VerificationCodeScreen} 
-    options={{headerShown: false}}/>
-
-    <Stack.Screen
-    name='resetPassword'
-    component={ResetPasswordScreen} 
-    options={{headerShown: false}}/>
+      <Stack.Screen
+        name='resetPassword'
+        component={ResetPasswordScreen}
+        options={{ headerShown: false }} />
 
     </Stack.Navigator>
   );
